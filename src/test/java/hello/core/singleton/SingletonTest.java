4bfd107b86ceaae96768eda1ed8da8f3b093e528
sonshn <sonshn202064@gmail.com>
2022-10-09 17:11:42 +0900

    싱글톤 패턴

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -25,4 +25,22 @@ public class SingletonTest {
         //두 객체는 서로 다르다.
         Assertions.assertThat(memberService1).isNotSameAs(memberService2);
     }
+
+    @Test
+    @DisplayName("싱글톤 패턴을 적용한 객체 사용")
+    void singletonServiceTest(){
+
+        //조회: 호출할 때 마다 같은 객체를 반환
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+
+        //참조값이 같은 것을 확인
+        System.out.println("singletonService1 = " + singletonService1);
+        System.out.println("singletonService2 = " + singletonService2);
+
+        // 두 객체는 서로 같다.
+        Assertions.assertThat(singletonService1).isSameAs(singletonService2);
+
+        singletonService1.logic();
+    }
 }
